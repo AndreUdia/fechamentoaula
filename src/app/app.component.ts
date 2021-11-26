11012a7a93d3834bde745e58c53dfbd221f0a8ef
Cabomaldade <soutom@yahoo.com.br>
2021-11-25 23:53:29 -0300

    Ng if Ng else com template e card com ngFor implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private flag: boolean = true;
+
+  exibirLista(){
+    return this.flag;
+  }
   
 }
